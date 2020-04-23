76dcb22855c455fb2dee85664e621fe33dcb895a
Raul Vagmacker <raulvagmacker17@gmail.com>
2020-04-23 14:05:54 -0300

    Estruturando projeto, criando as rotas do projeto e instalando o modulo de conexão do MySQL

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server')
 
-app.set('view engine', 'ejs');
+var routeNews = require('./app/routes/noticias');
+routeNews(app);
 
-app.get('/', function(req, res) {
-	res.render("home/index");
-});
+var routeHome = require('./app/routes/home');
+routehome(app);
 
-app.get('/formulario_inclusao_noticia', function(req, res) {
-	res.render("admin/form_add_noticia");
-});
-
-app.get('/noticias', function(req, res) {
-	res.render("noticias/noticias");
-});
+var routeFormNew = require('./app/routes/formulario_inclusao_noticias');
+routeFormNew(app);
 
 app.listen(3000, function() {
-	console.log("Servidor rodando com Express");
+	console.log('Servidor ON');
 });
