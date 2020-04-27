a6cd8a83de489ab9371c5c95cbb8695e1c0f8798
Raul Vagmacker <raulvagmacker17@gmail.com>
2020-04-27 20:48:46 -0300

    Refactoring da conexão com o banco de dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,19 +1,13 @@
+var database = require('../../config/database');
+
 module.exports = function(app) {
 
+	var connection = database();
+	
 	app.get('/noticias', function(req, res) {
 
-	var mysql = require('mysql');
-
-	var connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		database: 'portal_noticias'
-	});
-
-	connection.query('select * from noticias', function(error, result) {
-		res.send(result);
-	});
-	// res.render("noticias/noticias");
+		connection.query('select * from noticias', function(error, result) {
+	 		res.render("noticias/noticias", {noticias : result});
+		});
 	});
 };
