9dcb502a8e233a0aba86a486581ea46a05ac4a28
Raul Vagmacker <raulvagmacker17@gmail.com>
2020-05-04 19:15:31 -0300

    Instalando Express Validator no projeto, exibindo erros de validação com o mesmo e mantendo o preenchimento do formulário

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -3,9 +3,9 @@ module.exports = function(application) {
 	application.get('/noticias', function(req, res) {
 
 		var connection = application.config.database();
-		var noticiasModel = application.app.models.noticiasModel;
+		var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
-		noticiasModel.getNoticias(connection, function(error, result) {
+		noticiasModel.getNoticias(function(error, result) {
 	 		res.render("noticias/noticias", {noticias : result});
 		});
 	});
--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 var mysql = require('mysql');
 
 var dbMySQL = function() {
-	console.log('Conexao com o banco de dados foi estabelecida');
 		return mysql.createConnection({
 			host: 'localhost',
 			user: 'root',
@@ -10,6 +9,5 @@ var dbMySQL = function() {
 		});
 	}
 	module.exports = function() {
-		console.log('O autoload carregou o módulo de conexão com o banco de dados');
 		return dbMySQL;
 	}
