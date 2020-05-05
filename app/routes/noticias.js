b220d3c5c9f8292bc869724eb2004d599cf1ec26
Raul Vagmacker <raulvagmacker17@gmail.com>
2020-05-05 19:06:45 -0300

    Implementando designer pattern MVC no projeto

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,12 +1,10 @@
 module.exports = function(application) {
 	
 	application.get('/noticias', function(req, res) {
+		application.app.controllers.noticiasController.noticias(application, req, res);
+	});
 
-		var connection = application.config.database();
-		var noticiasModel = new application.app.models.NoticiasDAO(connection);
-
-		noticiasModel.getNoticias(function(error, result) {
-	 		res.render("noticias/noticias", {noticias : result});
-		});
+	application.get('/noticia', function(req, res) {
+		application.app.controllers.noticiasController.noticia(application, req, res);
 	});
 };
